fa5be8fe5e66fc69a494dd7f70a4cb2e2737a7e9
SanketPaharia <sanket.paharia@gmail.com>
2023-03-27 16:32:57 +0530

    'Address-entity'

--- CabApp/src/main/java/com/masai/CabApp/model/User.java
+++ CabApp/src/main/java/com/masai/CabApp/model/User.java
@@ -1,17 +1,25 @@
 package com.masai.CabApp.model;
 
 
+import lombok.Data;
+import lombok.Generated;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-
+@Data
 public class User {
 
+    @Id
+@GeneratedValue (strategy = GenerationType.IDENTITY)
     private int userId ;
 private String userName ;
 private String password ;
-private String Address ;
-private String mobileNumber ;
+private Address address ;
+    private String mobileNumber ;
 private String email ;
 
 
--- CabApp/src/main/java/com/masai/CabApp/model/Vehicle.java
+++ CabApp/src/main/java/com/masai/CabApp/model/Vehicle.java
@@ -1,10 +1,19 @@
 package com.masai.CabApp.model;
 
 import com.masai.CabApp.CabAppApplication;
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+@Data
 public class Vehicle {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int id ;
  //   private Enum type ;
     private String registrationNumber ;
